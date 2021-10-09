50af87999a3a21294b0c788a5f8b368c444f5c7f
Haswin Vidanage <haswin.v@ntucfirstcampus.com>
2021-10-09 21:19:56 +0800

    first commit

---
+++ server/server.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc_stream-local/server/sensor"
+	sensorpb "grpc_stream-local/server/sensorpb/protos"
+	"log"
+	"net"
+	"time"
+)
+
+
+type server struct {
+	Sensor *sensor.Sensor
+}
+
+func (s *server) TempSensor(req *sensorpb.SensorRequest,
+	stream sensorpb.Sensor_TempSensorServer) error {
+	for {
+		temp := s.Sensor.GetTempSensor()
+		err := stream.Send(&sensorpb.SensorResponse{Value: temp})
+		if err != nil {
+			log.Println("Error sending metric message ", err)
+		}
+		time.Sleep(time.Second * 5)
+	}
+	return nil
+}
+
+func (s *server) HumiditySensor(req *sensorpb.SensorRequest,
+	stream sensorpb.Sensor_HumiditySensorServer) error {
+
+	for {
+
+		humd := s.Sensor.GetHumiditySensor()
+
+		err := stream.Send(&sensorpb.SensorResponse{Value: humd})
+		if err != nil {
+			log.Println("Error sending metric message ", err)
+		}
+		time.Sleep(time.Second * 2)
+	}
+	return nil
+}
+
+var (
+	port int = 8080
+)
+
+func main() {
+
+	sns := sensor.NewSensor()
+
+	sns.StartMonitoring()
+
+	addr := fmt.Sprintf(":%d", port)
+
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Error while listening : %v", err)
+	}
+
+	s := grpc.NewServer()
+	sensorpb.RegisterSensorServer(s, &server{Sensor: sns})
+
+	log.Printf("Starting server in port :%d\n", port)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Error while serving : %v", err)
+	}
+
+}
